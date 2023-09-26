6aa4d8f9793aaf78aa3c709170fa7a9b6d7792d5
Kritikaadhikarii <itskritikaaa@gmail.com>
2023-09-26 18:04:15 +0545

    new changes made

--- index.js
+++ index.js
@@ -1,78 +1,98 @@
 document.addEventListener('DOMContentLoaded', function () {
+  let inputs = [];
   let currentInput = "";
-  let operator = "";
-  let firstInput = "";
 
   // Get display element
   const display = document.getElementById('display');
 
+  // Get history button element
+  const historyButton = document.getElementById('historyButton');
+
+  // Calculation history
+  const history = [];
+
   // Update display
   function updateDisplay(value) {
-      display.textContent = value;
+    display.textContent = value;
+  }
+
+  // Update history
+  function updateHistory() {
+    let historyText = history.map(entry => `${entry.expression} = ${entry.result}`).join('\n');
+    alert(`Calculation History:\n${historyText}`);
+  }
+
+  // Evaluate expression
+  function evaluateExpression() {
+    let result = inputs[0];
+    for (let i = 1; i < inputs.length; i += 2) {
+      const operator = inputs[i];
+      const operand = inputs[i + 1];
+
+      switch (operator) {
+        case '+':
+          result += operand;
+          break;
+        case '−':
+          result -= operand;
+          break;
+        case '×':
+          result *= operand;
+          break;
+        case '÷':
+          if (operand !== 0) {
+            result /= operand;
+          } else {
+            return 'Error';
+          }
+          break;
+      }
+    }
+    return result;
   }
 
   // Handle number input
   document.querySelectorAll('.number').forEach(button => {
-      button.addEventListener('click', function (e) {
-          currentInput += e.target.textContent;
-          updateDisplay(currentInput);
-      });
+    button.addEventListener('click', function (e) {
+      currentInput += e.target.textContent;
+      updateDisplay(currentInput);
+    });
   });
 
   // Handle operator input
   document.querySelectorAll('.operator').forEach(button => {
-      button.addEventListener('click', function (e) {
-          if (firstInput === "") {
-              firstInput = currentInput;
-              currentInput = "";
-              operator = e.target.textContent;
-          }
-      });
+    button.addEventListener('click', function (e) {
+      if (currentInput !== "") {
+        inputs.push(parseFloat(currentInput));
+        currentInput = "";
+      }
+
+      const operator = e.target.textContent;
+      if (operator !== "=") {
+        inputs.push(operator);
+      } else if (inputs.length >= 3) {
+        const result = evaluateExpression();
+        updateDisplay(result);
+
+        // Save to history
+        const expression = inputs.join(' ');
+        history.push({expression, result});
+
+        // Reset inputs and start a new calculation from the result
+        inputs = [result];
+      }
+    });
   });
 
   // Handle AC (All Clear)
   document.getElementById('clear').addEventListener('click', function () {
-      currentInput = "";
-      firstInput = "";
-      operator = "";
-      updateDisplay('0');
+    currentInput = "";
+    inputs = [];
+    updateDisplay('0');
   });
 
-  // Handle calculation (=)
-  document.querySelectorAll('.operator').forEach(button => {
-      if (button.textContent === "=") {
-          button.addEventListener('click', function () {
-              if (firstInput && currentInput && operator) {
-                  let result = 0;
-                  const num1 = parseFloat(firstInput);
-                  const num2 = parseFloat(currentInput);
-
-                  switch (operator) {
-                      case '+':
-                          result = num1 + num2;
-                          break;
-                      case '−':
-                          result = num1 - num2;
-                          break;
-                      case '×':
-                          result = num1 * num2;
-                          break;
-                      case '÷':
-                          if (num2 !== 0) {
-                              result = num1 / num2;
-                          } else {
-                              result = 'Error';
-                          }
-                          break;
-                      default:
-                          break;
-                  }
-
-                  updateDisplay(result);
-                  firstInput = result;
-                  currentInput = "";
-              }
-          });
-      }
+  // Handle history
+  historyButton.addEventListener('click', function () {
+    updateHistory();
   });
 });
